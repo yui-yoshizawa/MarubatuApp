e53df7d08d1f6100b6d57f5af59aeb58de7b5119
yui-yoshizawa <yui.yossy.0114@gmail.com>
2019-08-04 09:36:34 +0900

    問題表示処理の作成

--- MarubatuApp/ViewController.swift
+++ MarubatuApp/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // クイズの問題を表示
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    // クイズの問題を管理する
+    var currentQuestionNum: Int = 0
+    
+    // 問題を管理
+    let questions: [[String: Any]] = [
+        ["question": "iPhoneアプリを開発する統合環境はZcodeである",
+         "answer": false
+        ],
+        ["question": "Xcode画面の右側にはユーティリティーズがある",
+         "answer": true
+        ],
+        ["question": "UILabelは文字列を表示する際に利用する",
+         "answer": true
+        ],
+        ["question": "ドラえもんは常に地面から浮いている",
+         "answer": true
+        ],
+    ]
+    
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // 問題を表示する関数
+    func showQuestion() {
+        // currentQuestionNum(問題番号)の問題を取得
+        let question = questions[currentQuestionNum]
+        
+        //問題(辞書型)から Key を指定して内容を取得
+        if let que = question["question"] as? String {
+            // question Key の中身を Label に代入
+            questionLabel.text = que
+        }
+    }
 
-
+    // バツボタン
+    @IBAction func noButton(_ sender: Any) {
+    }
+    
+    // マルボタン
+    @IBAction func yesButton(_ sender: Any) {
+    }
+    
 }
 
